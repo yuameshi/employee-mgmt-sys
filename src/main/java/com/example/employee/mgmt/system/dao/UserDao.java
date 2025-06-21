3759c510405536372805703f68fa67e44255e6fe
糸 <64469437+yuameshi@users.noreply.github.com>
2025-06-21 20:42:01 +0800

    fix(UserDao): fix table name

--- src/main/java/com/example/employee/mgmt/system/dao/UserDao.java
+++ src/main/java/com/example/employee/mgmt/system/dao/UserDao.java
@@ -6,12 +6,16 @@ import com.example.employee.mgmt.system.entity.User;
 public class UserDao extends BaseDao {
 	public User getByCredentials(String identity, String password) {
 		BeanPropertyRowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
-		String sql = "SELECT * FROM users WHERE (username = ? OR user_id = ?) AND password = ?";
-		return jdbcTemplate.queryForObject(sql, rowMapper, identity, identity, password);
+		String sql = "SELECT * FROM user WHERE (username = ? OR user_id = ?) AND password = ? LIMIT 1";
+		try {
+			return jdbcTemplate.queryForObject(sql, rowMapper, identity, identity, password);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	public void updateLastLoginTime(Long userId) {
-		String sql = "UPDATE users SET last_login_time = NOW() WHERE user_id = ?";
+		String sql = "UPDATE user SET last_login_time = NOW() WHERE user_id = ?";
 		jdbcTemplate.update(sql, userId);
 	}
 }
